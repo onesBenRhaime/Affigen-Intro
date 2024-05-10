36c44b85729f75391c6c0aa297b27cf75a181aba
onesBenRhaime <onesrhaime28@gmail.com>
2024-05-10 17:32:57 +0100

    vf

--- src/App.tsx
+++ src/App.tsx
@@ -260,19 +260,8 @@ function App() {
 				<div className="container-fluid copyright bg-dark text-white-50 py-4">
 					<div className="container">
 						<div className="row">
-							<div className="col-md-6 text-center text-md-start">
-								<p className="mb-0">
-									© <a href="#">Your Site Name</a>. All Rights Reserved.
-								</p>
-							</div>
-							<div className="col-md-6 text-center text-md-end">
-								{/*/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. *** /*/}
-								<p className="mb-0">
-									Designed by <a href="https://htmlcodex.com">HTML Codex</a>
-									<br />
-									Distributed by <a href="https://themewagon.com">ThemeWagon</a>
-								</p>
-							</div>
+							<div className="col-md-6 text-center text-md-start"></div>
+							<div className="col-md-6 text-center text-md-end"></div>
 						</div>
 					</div>
 				</div>
